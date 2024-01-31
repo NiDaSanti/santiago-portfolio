20d0b260acac84db7370dc93d5b18e0ea1230ddc
NiDaSanti <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>
2024-01-30 19:49:39 -0800

    added hobby section

--- src/Components/Nfl/nfl.js
+++ src/Components/Nfl/nfl.js
@@ -3,6 +3,7 @@ import {Container, Row, Col, Card} from 'react-bootstrap/'
 
 const Nfl = ({nflStatement}) => {
   const [nflData, setNflData] = useState([])
+  const [loading, setLoading] = useState(false)
   const NFLURL = process.env.REACT_APP_SPORTSDATAIO_KEY
 
   const fetchAnUrl = async (url) => {
@@ -24,28 +25,32 @@ const Nfl = ({nflStatement}) => {
   }, [])
   return(
     <>
-      <h1>Hobbies.</h1>
-      <p>{nflStatement}</p>
-      <Row xs={2} md={4} lg={6}>
-        {nflData.map(svg => (
-          <Col key={svg.Key}>
-            <Card border="dark" style={{background: '#' + svg.PrimaryColor}}>
-              <Container fluid>
-                <Row className="p-2">
-                  <Col>
-                    <img src={svg.WikipediaLogoURL} alt={`${svg.City} ${svg.Name} Logo`} width="100" height="100" />
-                  </Col>
-                  <Col className="text-white">
-                    <div><i>{svg.FullName}</i></div>
-                    <div>{svg.Conference} {svg.Division}</div>
-                    <div>{svg.HeadCoach}</div>
-                  </Col>
-                </Row>
-              </Container>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+    {!loading ? ('Loading Data...') : (
+      <>
+        <h1>Hobbies.</h1>
+        <p>{nflStatement}</p>
+        <Row xs={2} md={4} lg={6}>
+          {nflData.map(svg => (
+            <Col key={svg.Key}>
+              <Card border="dark" style={{background: '#' + svg.PrimaryColor}}>
+                <Container fluid>
+                  <Row className="p-2">
+                    <Col>
+                      <img src={svg.WikipediaLogoURL} alt={`${svg.City} ${svg.Name} Logo`} width="100" height="100" />
+                    </Col>
+                    <Col className="text-white">
+                      <div><i>{svg.FullName}</i></div>
+                      <div>{svg.Conference} {svg.Division}</div>
+                      <div>{svg.HeadCoach}</div>
+                    </Col>
+                  </Row>
+                </Container>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </>
+    )}
     </>
   )
 }
