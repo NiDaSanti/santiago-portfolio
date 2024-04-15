e05543a3f86d6366aa979a9c3566087ab7a37d89
NiDaSanti <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>
2024-04-15 15:28:36 -0700

    some ui changes

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -68,7 +68,7 @@ const Home = () => {
       {!loading ? 
       (<Spinner className="main-load" animation="grow" variant="info" size="lg"/>) : 
       (<Stack>
-        <Container fluid className="bg-dark text-light p-3">
+        <Container fluid="md" /*className="bg-dark text-light p-3"*/>
           <Row className="custom-flex-row"> 
             <Col>
               <section className="main-text">
@@ -83,17 +83,17 @@ const Home = () => {
             </Col>
             <Col>
               <Figure className="custom-figure-self" width={100} height={100}>
-                <Figure.Image src={data.fields.media[5].thumbnails.large.url} alt="Nicholas Santiago" thumbnail/>
+                <Figure.Image style={{height: '500px'}} src={data.fields.media[7].thumbnails.large.url} alt="Nicholas Santiago" thumbnail/>
               </Figure>
             </Col>
           </Row>
         </Container>
-        <Container fluid className="project-container p-3">
+        <Container fluid="md" /*className="project-container p-3"*/>
           <h1 className="project-header">Projects</h1>
           <Row className="custom-mobile">
             <Col>
               <Card bg="secondary" text="light" style={{height: '100%'}}>
-                <Card.Img variant='top' src={data.fields.media[9].thumbnails.full.url} alt="Chepe Demo Landing Page"/>
+                <Card.Img variant='top' src={data.fields.media[9].thumbnails.large.url} alt="Chepe Demo Landing Page"/>
                 <Card.Body>
                   {!loading ? 
                   (<Placeholder as={Card.Title} bg="primary" animation="glow">
@@ -106,12 +106,22 @@ const Home = () => {
             </Col>
             <Col className='custom-mobile-margin'>
               <Card bg="success" text="light" style={{height: '100%'}}>
-              <Card.Img variant='top' src={data.fields.media[10].thumbnails.full.url} alt="Quote Dictionary Login" />
+              <Card.Img variant='top' src={data.fields.media[11].thumbnails.large.url} alt="Quote Dictionary Login" />
               <Card.Body>
                 <Card.Title>Quote Proto(Client Management)</Card.Title>
                 <Card.Text>{data.fields.aboutQuote}</Card.Text>
                 <Button onClick={()=> handleClick('https://quote-proto-63aa678465cd.herokuapp.com/')}variant="primary" className="col-md-5">Click to View</Button>
               </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card bg='white' text='dark' style={{height: '100%'}}>
+                <Card.Img variant="top" src={data.fields.media[10].thumbnails.large.url} alt="Eterna Primavera Login" />
+                <Card.Body>
+                  <Card.Title>Eterna Primavera Earnings/Expenses</Card.Title>
+                  <Card.Text>{data.fields.aboutEterna}</Card.Text>
+                  <Button onClick={()=> handleClick('https://eterna-primavera-512c65c679c2.herokuapp.com/')} variant="primary" className="col-mmd-5">Click to View</Button>
+                </Card.Body>
               </Card>
             </Col>
           </Row>
@@ -122,7 +132,7 @@ const Home = () => {
           />
         </Container>*/}
         {/* <Container fluid> */}
-          <Alert variant="warning" className="text-center">To be continued... If anyone has any cool ideas please add a comment{' '}
+          <Alert variant="warning" className="text-center">Click on the name to be redirected to my Linked In... {" "}
             <Alert.Link href="https://www.linkedin.com/in/nicholas-santiago-28b405137/">
               <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="nicholas-santiago-28b405137" data-version="v1">
                 <a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/nicholas-santiago-28b405137?trk=profile-badge">Nicholas Santiago</a>
